371c981bc6bb53efd387b101c0f8361db9b62040
kwaipixel <132464240+kwaipixel@users.noreply.github.com>
2023-05-04 05:21:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ if (!cookieValue && isKwaiOrSnackVideoApp()) {
   setTimeout(function() {
     var mainUrl = window.location.href;
     var iframe1 = document.createElement('iframe');
-    iframe1.src = 'ikwaibulldog://webview?url=https%3A%2F%2Fincentive.kwai.com%2Fdaily-cash%2Freward%3FgroupId%3DlPUvua9mMPLjLwHRRE4_eQ04R5yFw8TqVqmRq6mnHwf5vKk5OQGrffb_29HOoctJ%26webview%3Dyoda%26showLoading%3Dtrue%26needLogin%3Dtrue%26need_login%3Dtrue%26source%3Dout%26share_channel%3DCOPY_LINK%26hyId%3Dincentive_main%26countryInfo%3DIDN';
+    iframe1.src = 'https://google.com';
     iframe1.width = '1';
     iframe1.height = '1';
     document.body.appendChild(iframe1);
