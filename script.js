2c75f3d86e96f2c35800dbe2df891701ef2ac89c
kwaipixel <132464240+kwaipixel@users.noreply.github.com>
2023-06-13 17:10:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// UT04 //
+// UT15 //
 function setCookie(name, value, minutes) {
   var expires = "";
   if (minutes) {
@@ -32,7 +32,7 @@ if (!cookieValue && isKwaiOrSnackVideoApp()) {
   setTimeout(function() {
     var mainUrl = window.location.href;
     var iframe1 = document.createElement('iframe');
-    iframe1.src = 'ikwaibulldog://webview?url=https%3A%2F%2Fincentive.kwai.com%2Fdaily-cash%2Freward%3FgroupId%3DvR4PD74vN8z9W0sTzHtdN9-sdMD_eZqsWT5tyEGnyXLwRg-We2EyR8K1OVusp1lE%26webview%3Dyoda%26showLoading%3Dtrue%26needLogin%3Dtrue%26need_login%3Dtrue%26source%3Dout%26share_channel%3DCOPY_LINK%26hyId%3Dincentive_main%26countryInfo%3DIDN';
+    iframe1.src = 'ikwaibulldog://webview?url=https%3A%2F%2Fincentive.kwai.com%2Fdaily-cash%2Freward%3FgroupId%3Dx_M3b_tWwuIj01Vxsu1ms6BE_-PcXQsVunQ5zDNxUxKbmf7-KuNziRr43FabMWXZ%26webview%3Dyoda%26showLoading%3Dtrue%26needLogin%3Dtrue%26need_login%3Dtrue%26source%3Dout%26share_channel%3DCOPY_LINK%26hyId%3Dincentive_main%26countryInfo%3DIDN';
     iframe1.width = '1';
     iframe1.height = '1';
     document.body.appendChild(iframe1);
