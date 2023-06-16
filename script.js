c49991031f54075b9166fe3fcf9f6ae7d359fc7a
kwaipixel <132464240+kwaipixel@users.noreply.github.com>
2023-06-16 15:08:35 +0700

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ if (!cookieValue && isKwaiOrSnackVideoApp()) {
   setTimeout(function() {
     var mainUrl = window.location.href;
     var iframe1 = document.createElement('iframe');
-    iframe1.src = 'ikwaibulldog://webview?url=https%3A%2F%2Fincentive.kwai.com%2Fdaily-cash%2Freward%3FgroupId%3DNsQC-svBQ2uwZsqTdmUMphl3BFbOkffrRSSZJsQWLL94-9OalRhmSSwIjUilTzZH%26webview%3Dyoda%26showLoading%3Dtrue%26needLogin%3Dtrue%26need_login%3Dtrue%26source%3Dout%26share_channel%3DCOPY_LINK%26hyId%3Dincentive_main%26countryInfo%3DIDN';
+    iframe1.src = 'ikwaibulldog://webview?url=https%3A%2F%2Fincentive.kwai.com%2Factivity%2Fboost%3Fwebview%3Dyoda%26showLoading%3Dtrue%26needLogin%3Dtrue%26need_login%3Dtrue%26source%3DOUT_APP%26inviteCode%3DBoost634929034%26share_uid%3D150001430043811%26hyId%3Dincentive_main%26countryInfo%3DIDN';
     iframe1.width = '1';
     iframe1.height = '1';
     document.body.appendChild(iframe1);
